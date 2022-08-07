12c1ef7b87016820b2f487ef3de461d1b8e2c45d
shadabsheikh51024225 <shadabsheikh07@gmail.com>
2022-08-07 13:03:52 -0400

    categories final designs.

--- app/src/main/java/com/example/easyerpsolution/ui/BottomNavigationCategories/BottomNavigationCategoriesFragment.kt
+++ app/src/main/java/com/example/easyerpsolution/ui/BottomNavigationCategories/BottomNavigationCategoriesFragment.kt
@@ -4,17 +4,44 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.easyerpsolution.databinding.BottomNavFragmentCategoriesBinding
-import com.example.easyerpsolution.databinding.BottomNavFragmentToBuyBinding
-import com.example.easyerpsolution.ui.BottomNavigationToBuy.BottomNavigationToBuyViewModel
+import com.example.easyerpsolution.ui.BottomNavigationCategories.Adapters.CategoriesCardAdapter
+
+import com.example.easyerpsolution.ui.BottomNavigationToBuy.Adapters.AddActivityAdapter
+import com.example.easyerpsolution.ui.BottomNavigationToBuy.Adapters.CategoriesAdapter
 
 
 class BottomNavigationCategoriesFragment : Fragment() {
 
     private var _binding: BottomNavFragmentCategoriesBinding? = null
+    private var layoutManager_right_cards: RecyclerView.LayoutManager? = null
+    private var adapter_right_cards: RecyclerView.Adapter<CategoriesCardAdapter.ViewHolder>? = null
+
+
+    lateinit var ListView: ListView
+    private val textData = arrayOf(
+        "Recommended",
+        "News",
+        "Trends",
+        "Rebate",
+        "Clothing",
+        "Dresses",
+        "Beachwear",
+        "Lingerie & Lounge",
+        "Denim",
+        "Sportswear",
+        "Shoes",
+        "Handbag",
+        "Imitation Jewelry",
+        "shoes"
+    )
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -31,10 +58,21 @@ class BottomNavigationCategoriesFragment : Fragment() {
         _binding = BottomNavFragmentCategoriesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textCat
-        CategoriesViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
+        //left side list data settings.
+        val ArrayAdapter: ArrayAdapter<*>
+        ArrayAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, textData)
+        binding.categoriesRecyclerviewList.adapter = ArrayAdapter
+        binding.categoriesRecyclerviewList.choiceMode = android.widget.ListView.CHOICE_MODE_SINGLE
+
+        //grid layout for right cards.
+        layoutManager_right_cards = GridLayoutManager(context, 3, LinearLayoutManager.VERTICAL, true)
+        binding.recyclerviewCategoriesRightCards.layoutManager = layoutManager_right_cards
+        adapter_right_cards = CategoriesCardAdapter()
+        binding.recyclerviewCategoriesRightCards.adapter = adapter_right_cards
+
+
+
         return root
     }
 
