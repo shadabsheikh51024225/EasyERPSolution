01414c6fee27b4ed25f62afee9c44747a85bf7b7
shadabsheikh51024225 <shadabsheikh07@gmail.com>
2022-08-08 18:56:38 -0400

    staggered adapter working state. all done, very close oberservation required , due to current image size.

---
+++ app/src/main/java/com/example/easyerpsolution/ui/BottomNavigationGals/Adapters/Staggered_grid_apater.kt
@@ -0,0 +1,58 @@
+package com.example.easyerpsolution.ui.BottomNavigationGals.Adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.easyerpsolution.R
+
+
+class Staggered_grid_apater : RecyclerView.Adapter<Staggered_grid_apater.ViewHolder>() {
+    private val images = intArrayOf(
+        R.drawable.temp_gal_stag_one,
+        R.drawable.temp_gal_stag_two,
+        R.drawable.temp_gal_stag_one,
+        R.drawable.temp_gal_stag_two,
+        R.drawable.temp_gal_stag_one,
+        R.drawable.temp_gal_stag_two,
+        R.drawable.temp_gal_stag_one,
+        R.drawable.temp_gal_stag_two,
+        R.drawable.temp_gal_stag_one,
+        R.drawable.temp_gal_stag_two,
+        R.drawable.temp_gal_stag_one,
+        R.drawable.temp_gal_stag_two,
+        R.drawable.temp_gal_stag_one,
+        R.drawable.temp_gal_stag_two,
+        R.drawable.temp_gal_stag_one,
+        R.drawable.temp_gal_stag_two,
+        R.drawable.temp_gal_stag_one,
+        R.drawable.temp_gal_stag_two,
+
+    )
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        var itemImage: ImageView
+
+
+        init {
+            itemImage = itemView.findViewById(R.id.item_image_gal_staggered)
+
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val v = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.gal_staggered_grid_item, viewGroup, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+        viewHolder.itemImage.setImageResource(images[i])
+    }
+
+    override fun getItemCount(): Int {
+        return images.size
+    }
+}
