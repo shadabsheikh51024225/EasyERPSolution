1ba8293e9787a8593c1cc4ed068d5afaeb06e2db
shadabsheikh51024225 <shadabsheikh07@gmail.com>
2022-08-23 16:48:33 -0400

    shipping done

--- app/src/main/java/com/example/easyerpsolution/Shipping/ShippingActivity.kt
+++ app/src/main/java/com/example/easyerpsolution/Shipping/ShippingActivity.kt
@@ -1,12 +1,30 @@
 package com.example.easyerpsolution.Shipping
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import com.example.easyerpsolution.R
+import com.example.easyerpsolution.databinding.ActivityCartMainBinding
+import com.example.easyerpsolution.databinding.ActivityShippingBinding
 
 class ShippingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityShippingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shipping)
+        binding = ActivityShippingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //  setSupportActionBar(findViewById(R.id.main_tool_bar))
+        if (Build.VERSION.SDK_INT >= 21) {
+            val window = this.window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.statusBarColor = this.resources.getColor(R.color.Primary_color)
+        }
+    }
+    fun finish(view: View) {
+        finish()
     }
 }
